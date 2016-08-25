aa8ba07b697fd952d9380d6ec0400bf97bee2108
Rongbin <rongbin@amg.tv>
2016-08-25 15:14:07 -0400

    update

--- jobs/convergence.rb
+++ jobs/convergence.rb
@@ -1,4 +1,7 @@
+require 'elasticsearch'
 # Populate the graph with some random points
+
+client = Elasticsearch::Client.new log: true
 points = []
 (1..10).each do |i|
   points << { x: i, y: rand(50) }
@@ -6,9 +9,11 @@ end
 last_x = points.last[:x]
 
 SCHEDULER.every '2s' do
-  points.shift
-  last_x += 1
-  points << { x: last_x, y: rand(50) }
+  tweets_count_result = client.search index: 'stocks', type: 'Amazon', body: { size: 0, aggs: { count_by_type: { terms: { field: '_type'}}}}
+  tweets_count = tweets_count_result["aggregations"]["count_by_type"]["buckets"][0]["doc_count"]
+  # points.shift
+  # last_x += 1
+  # points << { x: last_x, y: rand(50) }
 
-  send_event('convergence', points: points)
+  send_event('convergence', points: tweets_count)
 end
--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,5 +1,5 @@
 require 'elasticsearch'
-
+require 'date'
 
 
 client = Elasticsearch::Client.new log: true
@@ -7,17 +7,46 @@ current_valuation = 0
 current_karma = 0
 
 SCHEDULER.every '2s' do
-  tweets_count_result = client.search index: 'stocks', type: 'Amazon', body: { size: 0, aggs: { count_by_type: { terms: { field: '_type'}}}}
-  tweets_count = tweets_count_result["aggregations"]["count_by_type"]["buckets"][0]["doc_count"]
-  amazon_scoring_result = client.search index: 'stocks', type: 'Amazon', body: { size: 0, aggs: { avg_grade: { avg: { field: 'scoring'}}}}
-  amazon_scoring = amazon_scoring_result["aggregations"]["avg_grade"]["value"]
-  last_valuation = current_valuation
-  last_karma     = current_karma
-  current_valuation = tweets_count
-  current_karma     = rand(200000)
+  date_now = Time.now.getutc.to_time.to_i
+  date_passed_24_hours =  (Time.now.getutc - (24*60*60)).to_time.to_i
+  date_three_hour = (Time.now.getutc - (3*60*60)).to_time.to_i
+
+  # Count how many tweets have been ingested
+  # tweets_count_result = client.search index: 'stocks', type: 'Amazon', body: { size: 0, aggs: { count_by_type: { terms: { field: '_type'}}}}
+  # tweets_count = tweets_count_result["aggregations"]["count_by_type"]["buckets"][0]["doc_count"]
+
+  # Scoring result for the past 24 hours
+  amazon_scoring_result_24p = client.search index: 'stocks', type: 'Amazon', body: { size: 0, query: {
+                                  range: {
+                                    created_at: {
+                                      gte: date_passed_24_hours,
+                                      lte: date_now
+                                    }
+                                  }
+                               },
+                              aggs: { avg_grade: { avg: { field: 'scoring'}}}}
+  amazon_scoring_24p = amazon_scoring_result["aggregations"]["avg_grade"]["value"]
+
+  #Scoring result for the past 3 hours
+
+  amazon_scoring_result_3p = client.search index: 'stocks', type: 'Amazon', body: { size: 0, query: {
+                                  range: {
+                                    created_at: {
+                                      gte: date_three_hour,
+                                      lte: date_now
+                                    }
+                                  }
+                               },
+                              aggs: { avg_grade: { avg: { field: 'scoring'}}}}
+  amazon_scoring_3p = amazon_scoring_result["aggregations"]["avg_grade"]["value"]
+  
+  last_valuation = amazon_scoring_24p*1000
+  last_karma     = amazon_scoring_24p
+  current_valuation = amazon_scoring_result_3p*1000
+  current_karma     = amazon_scoring_24p
 
   send_event('valuation', { current: current_valuation, last: last_valuation })
   send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: (amazon_scoring*100).round(2) })
+  send_event('synergy',   { value: (amazon_scoring_result_3p*100).round(2) })
 end
 
