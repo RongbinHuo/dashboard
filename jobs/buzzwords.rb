8d43ccde90fb106dbebbbb357f87253e259dc0d5
Rongbin <rongbin@amg.tv>
2016-08-25 21:03:23 -0400

    add one day change

--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -1,6 +1,6 @@
 require 'market_beat'
 
-buzzwords = ['RT Pirce', 'Day Range', 'Change Percent', 'After hour change'] 
+buzzwords = ['RT Pirce', 'Day Range', 'Change Percent', 'One day'] 
 buzzword_counts = Hash.new({ value: 0 })
 
 SCHEDULER.every '2s' do
@@ -11,6 +11,6 @@ SCHEDULER.every '2s' do
   buzzword_counts['Bid Pirce'] = { label: 'Bid Pirce', value: stock_price }
   buzzword_counts['Day Range'] = { label: 'Day Range', value: stock_day_range }
   buzzword_counts['Change Percent'] = { label: 'Change Percent', value: stock_change_percent }
-  buzzword_counts['One day price change and %'] = { label: 'One day price change and %', value: stock_one_day_change[0] + "  " + stock_one_day_change[1] }
+  buzzword_counts['One day'] = { label: 'One day', value: stock_one_day_change[0] + "  " + stock_one_day_change[1] }
   send_event('buzzwords', { items: buzzword_counts.values })
 end
