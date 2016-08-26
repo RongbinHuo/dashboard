07bd0e5207f485fd275c1d42ebc25c2c4e3da522
Rongbin <rongbin@amg.tv>
2016-08-25 21:49:26 -0400

    add one day change

--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -1,6 +1,6 @@
 require 'market_beat'
 
-buzzwords = ['RT Pirce', 'Day Range', 'Change Percent', 'One day', '52 Week High'] 
+buzzwords = ['RT Pirce', 'Day Range', 'Change Percent', 'One day', '52 Week High', '200 Day Change', '50 Day Change', 'Percent from 52 high'] 
 buzzword_counts = Hash.new({ value: 0 })
 
 SCHEDULER.every '2s' do
@@ -9,11 +9,17 @@ SCHEDULER.every '2s' do
   stock_change_percent = MarketBeat.change_percent_real_time :AMZN
   stock_one_day_change = MarketBeat.change_and_percent_change :AMZN
   year_high = MarketBeat.high_52_week :AMZN
+  two_hundrad_days_change = MarketBeat.percent_change_from_200_day_moving_average :AMZN
+  fifty_days_change = MarketBeat.percent_change_from_50_day_moving_average :AMZN
+  percent_from_year_high = MarketBeat.percent_change_from_52_week_high :AMZN
   buzzword_counts['Bid Pirce'] = { label: 'Bid Pirce', value: stock_price }
   buzzword_counts['Day Range'] = { label: 'Day Range', value: stock_day_range }
   buzzword_counts['Change Percent'] = { label: 'Change Percent', value: stock_change_percent }
   buzzword_counts['One day'] = { label: 'One day', value: stock_one_day_change[0] + "  " + stock_one_day_change[1] }
   buzzword_counts['52 Week High'] = { label: '52 Week High', value: year_high }
+  buzzword_counts['200 Day Change'] = { label: '200 Day Change', value: two_hundrad_days_change }
+  buzzword_counts['50 Day Change'] = { label: '50 Day Change', value: fifty_days_change }
+  buzzword_counts['Percent from 52 high'] = { label: 'Percent from 52 high', value: percent_from_year_high }
 
   send_event('buzzwords', { items: buzzword_counts.values })
 end
