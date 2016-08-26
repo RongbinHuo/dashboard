b7507cd52240d2d65bc09d45b83b41251e917c1f
Rongbin <rongbin@amg.tv>
2016-08-25 21:22:18 -0400

    add one day change

--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -8,11 +8,11 @@ SCHEDULER.every '2s' do
   stock_day_range = MarketBeat.days_range_real_time :AMZN
   stock_change_percent = MarketBeat.change_percent_real_time :AMZN
   stock_one_day_change = MarketBeat.change_and_percent_change :AMZN
-  # 52_week_high = MarketBeat.high_52_week :AMZN
+  52_week_high = MarketBeat.high_52_week :AMZN
   buzzword_counts['Bid Pirce'] = { label: 'Bid Pirce', value: stock_price }
   buzzword_counts['Day Range'] = { label: 'Day Range', value: stock_day_range }
   buzzword_counts['Change Percent'] = { label: 'Change Percent', value: stock_change_percent }
   buzzword_counts['One day'] = { label: 'One day', value: stock_one_day_change[0] + "  " + stock_one_day_change[1] }
-  # buzzword_counts['52 Week High'] = { label: '52 Week High', value: 52_week_high}
+  buzzword_counts['52 Week High'] = { label: '52 Week High', value: 52_week_high}
   send_event('buzzwords', { items: buzzword_counts.values })
 end
