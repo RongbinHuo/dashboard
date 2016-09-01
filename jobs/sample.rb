972f9e9307b39d2486c69ddbcab674c12f4c8fce
Rongbin <rongbin@amg.tv>
2016-09-01 10:38:09 -0400

    change

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -69,14 +69,14 @@ SCHEDULER.every '2s' do
     # predict_s = %x(python /home/ec2-user/twitt/predict.py -0.66410813 -0.66404772 /home/ec2-user/twitt/model/model.pkl)
     predict_s = `python /home/ec2-user/twitt/predict_decision_tree.py #{scoring_increase_overall} #{scoring_increase_than_pre} #{quote_data_year_percent} #{quote_data_day_percent} /home/ec2-user/twitt/model/decision_tree.pkl`
     score_percent = predict_s.to_f
-    predict = 0
-    if score_percent > 0.3
-      predict = score_percent/0.5
-    elsif score_percent < 0.2
-      predict = -(0.2-score_percent)/0.3
-    else
-      predict = (score_percent-0.25)/0.1
-    end
+    predict = predict_s.to_f
+    # if score_percent > 0.3
+    #   predict = score_percent/0.5
+    # elsif score_percent < 0.2
+    #   predict = -(0.2-score_percent)/0.3
+    # else
+    #   predict = (score_percent-0.25)/0.1
+    # end
   else
     predict_s = `python /home/ec2-user/twitt/predict_decision_tree.py 0 0 0 0 /home/ec2-user/twitt/model/decision_tree.pkl`
     predict = predict_s.to_f
