1ed3be86b024d1c5c06f936908edce9ea0482a18
Rongbin <rongbin@amg.tv>
2016-09-21 22:37:11 -0400

    fetch latest twit

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -17,10 +17,17 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
     tweets = twitter.search("#{search_term}")
 
     if tweets
-      tweets = tweets.map do |tweet|
-        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
+      tweets_ary = twitter.search("#{search_term}").first(5)
+      content = ''
+      tweets_ary.each do |t|
+        if t
+          content << t.text+'   '+t.created_at.to_s+"\n"
+        end
       end
-      send_event('twitter_mentions', text: tweets)
+      # tweets = tweets.map do |tweet|
+      #   { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https,  }
+      # end
+      send_event('twitter_mentions', text: content)
     end
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
