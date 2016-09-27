8e26aa2f5f9257b8b8b4626f62bcb6cee5215780
Rongbin <rongbin@amg.tv>
2016-09-26 22:50:37 -0400

    add news

--- jobs/web_scraper.rb
+++ jobs/web_scraper.rb
@@ -1,9 +1,28 @@
 require 'open-uri'
 require 'nokogiri'
 
+url = 'https://www.bulliondesk.com/gold-news/'
 
+SCHEDULER.every '2s' do
+  html = open(url)
+  doc = Nokogiri::HTML(html)
+  doc.css('artical')
 
+  latest_news_content = doc.css('section#news-top h2').text.strip()
+  latest_new_timestamp = doc.css('section#news-top div.meta').text.strip()
+  latest_news_url = doc.css('section#news-top a')[0]['href'].strip()
 
-url = 'http://www.cubecinema.com/programme'
-html = open(url)
-doc = Nokogiri::HTML(html)
+  basic_news1_content = doc.css('section#news-feature article h4')[0].text.strip()
+  basic_news1_timestamp = doc.css('section#news-feature article div.meta')[0].text.strip()
+  basic_news1_url = doc.css('section#news-feature article h4 a')[0]['href'].strip()
+
+  basic_news2_content = doc.css('section#news-feature').css("article")[1].css("h4").text.strip()
+  basic_news2_timestamp = doc.css('section#news-feature').css("article")[1].css("div.meta").text.strip()
+  basic_news2_url = doc.css('section#news-feature article h4 a')[1]['href'].strip()
+  
+  news['latest'] = { content: latest_news_content, time: latest_new_timestamp, url:  latest_news_url}
+  news['basic1'] = { content: basic_news1_content, time: basic_news1_timestamp, url:  basic_news1_url}
+  news['basic2'] = { content: basic_news2_content, time: basic_news2_timestamp, url:  basic_news2_url}
+  
+  send_event('gold_news', items: news)
+end
