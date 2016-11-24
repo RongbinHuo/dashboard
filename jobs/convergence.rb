17c5e99c6646dd6ac7a072385b016251b3d4bcc8
Rongbin <rongbin@amg.tv>
2016-11-24 15:34:33 -0500

    remove es

--- jobs/convergence.rb
+++ jobs/convergence.rb
@@ -1,16 +1,25 @@
-require 'elasticsearch'
-# Populate the graph with some random points
+require 'pony'
+require 'sanitize'
+require 'mysql'
 
-client = Elasticsearch::Client.new log: false
+db_host  = "rongbin.cdpxz2jepyxw.us-east-1.rds.amazonaws.com"
+db_user  = "root"
+db_pass  = "12345678"
+db_name = "twit"
+
+# Populate the graph with some random points
 points = []
 (1..10).each do |i|
-  points << { x: i, y: rand(50) }
+  points << { x: i, y: rand(5) }
 end
 last_x = points.last[:x]
 
 SCHEDULER.every '2s' do
-  tweets_count_result = client.search index: 'stocks', type: 'DUST', body: { size: 0, aggs: { count_by_type: { terms: { field: '_type'}}}}
-  tweets_count = tweets_count_result["aggregations"]["count_by_type"]["buckets"][0]["doc_count"]
+  conn = Mysql.new(db_host, db_user, db_pass, db_name)
+  ts = (Time.now - (24*60*60)).strftime('%Y-%m-%d %H:%M:%S')
+  rs = conn.query("SELECT COUNT(*) from gold_news where create_at > '#{ts}' ")
+  tweets_count = rs.fetch_row[0].to_i
+
   points.shift
   last_x += 1
   points << { x: last_x, y: tweets_count }
