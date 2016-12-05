3923420b88193c4c010fada876ff5ec2eb22c1d3
Rongbin <rongbin@amg.tv>
2016-12-05 16:29:01 -0500

    add gold chart

--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -3,8 +3,8 @@ require 'market_beat'
 buzzwords = ['RT Pirce', 'Day Range', 'Change Percent', 'One day', '52 Week High', '200 Day Change', '50 Day Change', 'Year high change'] 
 buzzword_counts = Hash.new({ value: 0 })
 
-SCHEDULER.every '2s' do
-  stock_price = MarketBeat.last_trade_real_time :DUST
+SCHEDULER.every '60s' do
+  stock_price = MarketBeat.bid :DUST
   stock_day_range = MarketBeat.days_range_real_time :DUST
   stock_change_percent = MarketBeat.percent_change_real_time :DUST
   stock_one_day_change = MarketBeat.change_and_percent_change :DUST
@@ -14,12 +14,12 @@ SCHEDULER.every '2s' do
   percent_from_year_high = MarketBeat.percent_change_from_52_week_high :DUST
   buzzword_counts['Bid Pirce'] = { label: 'Bid Pirce', value: stock_price }
   # buzzword_counts['Day Range'] = { label: 'Day Range', value: stock_day_range }
-  buzzword_counts['Change Percent'] = { label: 'Change Percent', value: stock_change_percent }
-  buzzword_counts['One day'] = { label: 'One day', value: stock_one_day_change[1] }
-  buzzword_counts['52 Week High'] = { label: '52 Week High', value: year_high }
-  buzzword_counts['200 Day Change'] = { label: '200 Day Change', value: two_hundrad_days_change }
-  buzzword_counts['50 Day Change'] = { label: '50 Day Change', value: fifty_days_change }
-  buzzword_counts['Year high change'] = { label: 'Year high change', value: percent_from_year_high }
+  # buzzword_counts['Change Percent'] = { label: 'Change Percent', value: stock_change_percent }
+  buzzword_counts['Day Change'] = { label: 'One day', value: stock_one_day_change[1] }
+  # buzzword_counts['52 Week High'] = { label: '52 Week High', value: year_high }
+  # buzzword_counts['200 Day Change'] = { label: '200 Day Change', value: two_hundrad_days_change }
+  # buzzword_counts['50 Day Change'] = { label: '50 Day Change', value: fifty_days_change }
+  # buzzword_counts['Year high change'] = { label: 'Year high change', value: percent_from_year_high }
   
   send_event('buzzwords', { items: buzzword_counts.values })
 end
