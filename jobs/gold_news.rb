6b804162374071fe12970c87090199dbaa47c3d6
Rongbin <rongbin@amg.tv>
2017-02-27 23:23:04 -0500

    handle exception

--- jobs/gold_news.rb
+++ jobs/gold_news.rb
@@ -30,120 +30,134 @@ SCHEDULER.every '300s' do
 	insert = conn.prepare('INSERT INTO gold_news (text, link) VALUES(?, ?)')
 	insert_with_datetime = conn.prepare('INSERT INTO gold_news (text, link, news_date) VALUES(?, ?, ?)')
 
-	html_usagold = open(url_usagold)
-	doc_usagold = Nokogiri::HTML(html_usagold)
-	news_time_source_usagold = doc_usagold.css('span.txtarial11 table tr td[valign="TOP"]')
-	news_time_source_usagold.each do |n|
-		if n["align"].nil?
-		    news_text = n.text.strip()
-			href = n.css('font a')[0]['href'].strip()
-			rs = check_query.execute(href).fetch
-			if rs.nil?
-			    insert.execute(news_text, href)
+	begin
+		html_usagold = open(url_usagold)
+		doc_usagold = Nokogiri::HTML(html_usagold)
+		news_time_source_usagold = doc_usagold.css('span.txtarial11 table tr td[valign="TOP"]')
+		news_time_source_usagold.each do |n|
+			if n["align"].nil?
+			    news_text = n.text.strip()
+				href = n.css('font a')[0]['href'].strip()
+				rs = check_query.execute(href).fetch
+				if rs.nil?
+				    insert.execute(news_text, href)
+				end
 			end
 		end
-	end
+	rescue; end
 
-	agent = Mechanize.new
-	page = agent.get url_kitco
-	# html_kitco = open(url_kitco)
-	doc_kitco = Nokogiri::HTML(page.content)
-	news_time_source_kitco = doc_kitco.css('div .gold')
-	news_time_source_kitco.each do |n|
-	    news_text = n.css('.article-title').text.strip()
-	    news_time_str = n.css('.post-date').text.strip()
-	    news_time = Time.parse(news_time_str)
-	    raw_href = n.css('a')[0]['href'].strip()
-	    news_href = raw_href
-	    if raw_href.start_with?('/')
-	    	news_href = 'http://www.kitco.com'+raw_href
-	    end
-	    rs = check_query.execute(news_href).fetch
-	    if rs.nil?
-	    	insert_with_datetime.execute(news_text, news_href, news_time)
-	    end
-	end
-
-	html_investing = open(url_investing)
-	doc_investing = Nokogiri::HTML(html_investing)
-	news_time_source_investing = doc_investing.css('div .mediumTitle1 .articleItem')
-	news_time_source_investing.each do |n|
-		news_entry = n.css('div')[0].css('a')
-		news_text = news_entry.text.strip()
-		raw_href = news_entry[0]['href'].strip()
-		news_href = raw_href
-		if raw_href.start_with?('/')
-			news_href = 'https://www.investing.com'+raw_href
+	begin
+		agent = Mechanize.new
+		page = agent.get url_kitco
+		# html_kitco = open(url_kitco)
+		doc_kitco = Nokogiri::HTML(page.content)
+		news_time_source_kitco = doc_kitco.css('div .gold')
+		news_time_source_kitco.each do |n|
+		    news_text = n.css('.article-title').text.strip()
+		    news_time_str = n.css('.post-date').text.strip()
+		    news_time = Time.parse(news_time_str)
+		    raw_href = n.css('a')[0]['href'].strip()
+		    news_href = raw_href
+		    if raw_href.start_with?('/')
+		    	news_href = 'http://www.kitco.com'+raw_href
+		    end
+		    rs = check_query.execute(news_href).fetch
+		    if rs.nil?
+		    	insert_with_datetime.execute(news_text, news_href, news_time)
+		    end
 		end
-		rs = check_query.execute(news_href).fetch
-		if rs.nil?
-	    	insert.execute(news_text, news_href)
-	    end
-	end
+	rescue; end
 
-	html_bullionvault = open(url_bullionvault)
-	doc_bullionvault = Nokogiri::HTML(html_bullionvault)
-	news_bullionvault = doc_bullionvault.css('.view-content table tbody tr')
-	news_bullionvault.each do |n|
-		news_text = n.css('td a').text.strip
-		raw_href = n.css('td a')[0]['href'].strip()
-		news_href = raw_href
-		if raw_href.start_with?('/')
-			news_href = 'https://www.bullionvault.com'+raw_href
+	begin
+		html_investing = open(url_investing)
+		doc_investing = Nokogiri::HTML(html_investing)
+		news_time_source_investing = doc_investing.css('div .mediumTitle1 .articleItem')
+		news_time_source_investing.each do |n|
+			news_entry = n.css('div')[0].css('a')
+			news_text = news_entry.text.strip()
+			raw_href = news_entry[0]['href'].strip()
+			news_href = raw_href
+			if raw_href.start_with?('/')
+				news_href = 'https://www.investing.com'+raw_href
+			end
+			rs = check_query.execute(news_href).fetch
+			if rs.nil?
+		    	insert.execute(news_text, news_href)
+		    end
 		end
-		rs = check_query.execute(news_href).fetch
-		if rs.nil?
-	    	insert.execute(news_text, news_href)
-	    end
-	end
+	rescue; end
 
-	html_silverdoctors = open(url_silverdoctors)
-	doc_silverdoctors = Nokogiri::HTML(html_silverdoctors)
-	news_silverdoctors = doc_silverdoctors.css('div#archive-content-column div.index-article')
-	news_silverdoctors.each do |n|
-		news_text = n.css('.page-title a').text.strip
-		raw_href = n.css('.page-title a')[0]['href'].strip()
-		news_href = raw_href
-		if raw_href.start_with?('/')
-			news_href = 'http://www.silverdoctors.com/gold/gold-news'+raw_href
+	begin
+		html_bullionvault = open(url_bullionvault)
+		doc_bullionvault = Nokogiri::HTML(html_bullionvault)
+		news_bullionvault = doc_bullionvault.css('.view-content table tbody tr')
+		news_bullionvault.each do |n|
+			news_text = n.css('td a').text.strip
+			raw_href = n.css('td a')[0]['href'].strip()
+			news_href = raw_href
+			if raw_href.start_with?('/')
+				news_href = 'https://www.bullionvault.com'+raw_href
+			end
+			rs = check_query.execute(news_href).fetch
+			if rs.nil?
+		    	insert.execute(news_text, news_href)
+		    end
 		end
-		rs = check_query.execute(news_href).fetch
-		if rs.nil?
-	    	insert.execute(news_text, news_href)
-	    end
-	end
+	rescue; end
 
-	html_cnbc = open(url_cnbc)
-	doc_cnbc = Nokogiri::HTML(html_cnbc)
-	news_cnbc = doc_cnbc.css('div.stories-lineup li')
-	news_cnbc.each do |n|
-		news_text = n.css('.headline').text.strip
-		raw_href = n.css('.headline a')[0]['href'].strip()
-		news_href = raw_href
-		if raw_href.start_with?('/')
-			news_href = 'http://www.cnbc.com'+raw_href
+	begin
+		html_silverdoctors = open(url_silverdoctors)
+		doc_silverdoctors = Nokogiri::HTML(html_silverdoctors)
+		news_silverdoctors = doc_silverdoctors.css('div#archive-content-column div.index-article')
+		news_silverdoctors.each do |n|
+			news_text = n.css('.page-title a').text.strip
+			raw_href = n.css('.page-title a')[0]['href'].strip()
+			news_href = raw_href
+			if raw_href.start_with?('/')
+				news_href = 'http://www.silverdoctors.com/gold/gold-news'+raw_href
+			end
+			rs = check_query.execute(news_href).fetch
+			if rs.nil?
+		    	insert.execute(news_text, news_href)
+		    end
 		end
-		rs = check_query.execute(news_href).fetch
-		if rs.nil?
-	    	insert.execute(news_text, news_href)
-	    end
-	end
+	rescue; end
 
-	html_economictimes = open(url_economictimes)
-	doc_economictimes = Nokogiri::HTML(html_economictimes)
-	news_economictimes = doc_economictimes.css('li#all div')
-	news_economictimes.each do |n|
-		news_text = n.css('a h3').text.strip
-		raw_href = n.css('a')[0]['href'].strip()
-		news_href = raw_href
-		if raw_href.start_with?('/')
-			news_href = 'http://economictimes.indiatimes.com'+raw_href
+	begin
+		html_cnbc = open(url_cnbc)
+		doc_cnbc = Nokogiri::HTML(html_cnbc)
+		news_cnbc = doc_cnbc.css('div.stories-lineup li')
+		news_cnbc.each do |n|
+			news_text = n.css('.headline').text.strip
+			raw_href = n.css('.headline a')[0]['href'].strip()
+			news_href = raw_href
+			if raw_href.start_with?('/')
+				news_href = 'http://www.cnbc.com'+raw_href
+			end
+			rs = check_query.execute(news_href).fetch
+			if rs.nil?
+		    	insert.execute(news_text, news_href)
+		    end
 		end
-		rs = check_query.execute(news_href).fetch
-		if rs.nil?
-	    	insert.execute(news_text, news_href)
-	    end
-	end
+	rescue; end
+
+	begin
+		html_economictimes = open(url_economictimes)
+		doc_economictimes = Nokogiri::HTML(html_economictimes)
+		news_economictimes = doc_economictimes.css('li#all div')
+		news_economictimes.each do |n|
+			news_text = n.css('a h3').text.strip
+			raw_href = n.css('a')[0]['href'].strip()
+			news_href = raw_href
+			if raw_href.start_with?('/')
+				news_href = 'http://economictimes.indiatimes.com'+raw_href
+			end
+			rs = check_query.execute(news_href).fetch
+			if rs.nil?
+		    	insert.execute(news_text, news_href)
+		    end
+		end
+	rescue; end
 
 	conn.close
 end
